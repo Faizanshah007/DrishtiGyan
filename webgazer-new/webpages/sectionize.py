7fa5d255e3b8e484ab2c54be7813835c64a85c0e
Faizanshah Ansari <faizanshah.ansari@gmail.com>
2020-03-23 22:26:23 +0530

    Add files via upload

--- webgazer-new/webpages/sectionize.py
+++ webgazer-new/webpages/sectionize.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import os, time
+#import os
 import pyautogui
 import pygetwindow as gw
 import pythoncom, PyHook3
