42f0a729332abafa971c8232e6e076cbc65f8354
Faizanshah Ansari <faizanshah.ansari@gmail.com>
2021-01-11 22:34:10 +0530

    Delete run.php

--- webgazer-new/run.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $log_file_name = 'mylog.log'; // Change to the log file name
-  $data = isset($_REQUEST['g'])?$_REQUEST['g']:"";
-  file_put_contents($log_file_name, $data, FILE_APPEND);
-  header('Location: /'); // redirect back to the main site
-?>
